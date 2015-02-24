e6070eef4f1f477313603fdc8c7d781023f1100c
Miguel Rivas <mrivas616@hotmail.com>
2015-02-23 21:06:57 -0600

    Agregue form para mantenimientos

---
+++ axy/Mantenimientos.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace axy
+{
+    partial class MantProductosForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gvMntProductos = new System.Windows.Forms.DataGridView();
+            this.btnAgregarProducto = new System.Windows.Forms.Button();
+            this.btnEditarProducto = new System.Windows.Forms.Button();
+            this.btnEliminarProducto = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gvMntProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gvMntProductos
+            // 
+            this.gvMntProductos.AllowUserToAddRows = false;
+            this.gvMntProductos.AllowUserToDeleteRows = false;
+            this.gvMntProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvMntProductos.Location = new System.Drawing.Point(13, 13);
+            this.gvMntProductos.Name = "gvMntProductos";
+            this.gvMntProductos.ReadOnly = true;
+            this.gvMntProductos.Size = new System.Drawing.Size(536, 199);
+            this.gvMntProductos.TabIndex = 0;
+            // 
+            // btnAgregarProducto
+            // 
+            this.btnAgregarProducto.Location = new System.Drawing.Point(312, 264);
+            this.btnAgregarProducto.Name = "btnAgregarProducto";
+            this.btnAgregarProducto.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregarProducto.TabIndex = 1;
+            this.btnAgregarProducto.Text = "Agregar";
+            this.btnAgregarProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnEditarProducto
+            // 
+            this.btnEditarProducto.Location = new System.Drawing.Point(393, 264);
+            this.btnEditarProducto.Name = "btnEditarProducto";
+            this.btnEditarProducto.Size = new System.Drawing.Size(75, 23);
+            this.btnEditarProducto.TabIndex = 2;
+            this.btnEditarProducto.Text = "Editar";
+            this.btnEditarProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarProducto
+            // 
+            this.btnEliminarProducto.Location = new System.Drawing.Point(474, 264);
+            this.btnEliminarProducto.Name = "btnEliminarProducto";
+            this.btnEliminarProducto.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarProducto.TabIndex = 3;
+            this.btnEliminarProducto.Text = "Eliminar";
+            this.btnEliminarProducto.UseVisualStyleBackColor = true;
+            // 
+            // MantProductosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 309);
+            this.Controls.Add(this.btnEliminarProducto);
+            this.Controls.Add(this.btnEditarProducto);
+            this.Controls.Add(this.btnAgregarProducto);
+            this.Controls.Add(this.gvMntProductos);
+            this.Name = "MantProductosForm";
+            this.Text = "Mantenimiento";
+            ((System.ComponentModel.ISupportInitialize)(this.gvMntProductos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gvMntProductos;
+        private System.Windows.Forms.Button btnAgregarProducto;
+        private System.Windows.Forms.Button btnEditarProducto;
+        private System.Windows.Forms.Button btnEliminarProducto;
+    }
+}
