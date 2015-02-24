eeaf80efe9644c3bcd606d902ddeeff14a3d8310
Miguel Rivas <mrivas616@hotmail.com>
2015-02-23 20:28:24 -0600

    First commit, Form principal con menu y form de mantenimiento de productos vacio

---
+++ axy/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace axy
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.detallesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.empleadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntVendedoresMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntCoordinadoresMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntProductosMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntProveedoresMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntComisionesMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.detallesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.detalVendedoresMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.detalCoordinadoresMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.detalVentasMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.comisionesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.detalComVentasMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.detalComFacturasMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.detalOrdenesMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntComisionesVentasMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntComisionesFacturasMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.detallesToolStripMenuItem,
+            this.detallesToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(640, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // detallesToolStripMenuItem
+            // 
+            this.detallesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.empleadosToolStripMenuItem,
+            this.mntProductosMenu,
+            this.mntProveedoresMenu,
+            this.mntComisionesMenu});
+            this.detallesToolStripMenuItem.Name = "detallesToolStripMenuItem";
+            this.detallesToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.detallesToolStripMenuItem.Text = "Mantenimiento";
+            // 
+            // empleadosToolStripMenuItem
+            // 
+            this.empleadosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mntVendedoresMenu,
+            this.mntCoordinadoresMenu});
+            this.empleadosToolStripMenuItem.Name = "empleadosToolStripMenuItem";
+            this.empleadosToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.empleadosToolStripMenuItem.Text = "Empleados";
+            // 
+            // mntVendedoresMenu
+            // 
+            this.mntVendedoresMenu.Name = "mntVendedoresMenu";
+            this.mntVendedoresMenu.Size = new System.Drawing.Size(152, 22);
+            this.mntVendedoresMenu.Text = "Vendedores";
+            // 
+            // mntCoordinadoresMenu
+            // 
+            this.mntCoordinadoresMenu.Name = "mntCoordinadoresMenu";
+            this.mntCoordinadoresMenu.Size = new System.Drawing.Size(152, 22);
+            this.mntCoordinadoresMenu.Text = "Coordinadores";
+            // 
+            // mntProductosMenu
+            // 
+            this.mntProductosMenu.Name = "mntProductosMenu";
+            this.mntProductosMenu.Size = new System.Drawing.Size(152, 22);
+            this.mntProductosMenu.Text = "Productos";
+            this.mntProductosMenu.Click += new System.EventHandler(this.mntProductosMenu_Click);
+            // 
+            // mntProveedoresMenu
+            // 
+            this.mntProveedoresMenu.Name = "mntProveedoresMenu";
+            this.mntProveedoresMenu.Size = new System.Drawing.Size(152, 22);
+            this.mntProveedoresMenu.Text = "Proveedores";
+            // 
+            // mntComisionesMenu
+            // 
+            this.mntComisionesMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mntComisionesVentasMenu,
+            this.mntComisionesFacturasMenu});
+            this.mntComisionesMenu.Name = "mntComisionesMenu";
+            this.mntComisionesMenu.Size = new System.Drawing.Size(152, 22);
+            this.mntComisionesMenu.Text = "Comisiones";
+            // 
+            // detallesToolStripMenuItem1
+            // 
+            this.detallesToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.detalVendedoresMenu,
+            this.detalCoordinadoresMenu,
+            this.detalVentasMenu,
+            this.comisionesToolStripMenuItem1,
+            this.detalOrdenesMenu});
+            this.detallesToolStripMenuItem1.Name = "detallesToolStripMenuItem1";
+            this.detallesToolStripMenuItem1.Size = new System.Drawing.Size(60, 20);
+            this.detallesToolStripMenuItem1.Text = "Detalles";
+            // 
+            // detalVendedoresMenu
+            // 
+            this.detalVendedoresMenu.Name = "detalVendedoresMenu";
+            this.detalVendedoresMenu.Size = new System.Drawing.Size(152, 22);
+            this.detalVendedoresMenu.Text = "Vendedores";
+            // 
+            // detalCoordinadoresMenu
+            // 
+            this.detalCoordinadoresMenu.Name = "detalCoordinadoresMenu";
+            this.detalCoordinadoresMenu.Size = new System.Drawing.Size(152, 22);
+            this.detalCoordinadoresMenu.Text = "Coordinadores";
+            // 
+            // detalVentasMenu
+            // 
+            this.detalVentasMenu.Name = "detalVentasMenu";
+            this.detalVentasMenu.Size = new System.Drawing.Size(152, 22);
+            this.detalVentasMenu.Text = "Ventas";
+            // 
+            // comisionesToolStripMenuItem1
+            // 
+            this.comisionesToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.detalComVentasMenu,
+            this.detalComFacturasMenu});
+            this.comisionesToolStripMenuItem1.Name = "comisionesToolStripMenuItem1";
+            this.comisionesToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.comisionesToolStripMenuItem1.Text = "Comisiones";
+            // 
+            // detalComVentasMenu
+            // 
+            this.detalComVentasMenu.Name = "detalComVentasMenu";
+            this.detalComVentasMenu.Size = new System.Drawing.Size(191, 22);
+            this.detalComVentasMenu.Text = "Ventas";
+            // 
+            // detalComFacturasMenu
+            // 
+            this.detalComFacturasMenu.Name = "detalComFacturasMenu";
+            this.detalComFacturasMenu.Size = new System.Drawing.Size(191, 22);
+            this.detalComFacturasMenu.Text = "Recuperacion facturas";
+            // 
+            // detalOrdenesMenu
+            // 
+            this.detalOrdenesMenu.Name = "detalOrdenesMenu";
+            this.detalOrdenesMenu.Size = new System.Drawing.Size(152, 22);
+            this.detalOrdenesMenu.Text = "Ordenes";
+            // 
+            // mntComisionesVentasMenu
+            // 
+            this.mntComisionesVentasMenu.Name = "mntComisionesVentasMenu";
+            this.mntComisionesVentasMenu.Size = new System.Drawing.Size(191, 22);
+            this.mntComisionesVentasMenu.Text = "Ventas";
+            // 
+            // mntComisionesFacturasMenu
+            // 
+            this.mntComisionesFacturasMenu.Name = "mntComisionesFacturasMenu";
+            this.mntComisionesFacturasMenu.Size = new System.Drawing.Size(191, 22);
+            this.mntComisionesFacturasMenu.Text = "Recuperacion facturas";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 300);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainForm";
+            this.Text = "AXY desktop application";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem detallesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem empleadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mntProductosMenu;
+        private System.Windows.Forms.ToolStripMenuItem mntProveedoresMenu;
+        private System.Windows.Forms.ToolStripMenuItem detallesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem detalVendedoresMenu;
+        private System.Windows.Forms.ToolStripMenuItem detalCoordinadoresMenu;
+        private System.Windows.Forms.ToolStripMenuItem comisionesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem detalComVentasMenu;
+        private System.Windows.Forms.ToolStripMenuItem detalComFacturasMenu;
+        private System.Windows.Forms.ToolStripMenuItem detalVentasMenu;
+        private System.Windows.Forms.ToolStripMenuItem detalOrdenesMenu;
+        private System.Windows.Forms.ToolStripMenuItem mntVendedoresMenu;
+        private System.Windows.Forms.ToolStripMenuItem mntCoordinadoresMenu;
+        private System.Windows.Forms.ToolStripMenuItem mntComisionesMenu;
+        private System.Windows.Forms.ToolStripMenuItem mntComisionesVentasMenu;
+        private System.Windows.Forms.ToolStripMenuItem mntComisionesFacturasMenu;
+    }
+}
+
